0917d8a597956817557a1325044a64691a785a3d
KalekyeRaychelle <kalekyeraychelle@gmail.com>
2026-02-26 19:04:13 +0200

    Change the header to match the new design. - Removed the old CSS file and create a new CSS module for the header component. - Added initials instead of the full name in the header. - Updated the header component to use the new CSS module.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Header.module.css';
 
-import '../styles/Header.css';
-const Header = () => {
+const Navbar = () => {
   return (
-    <div className='header'>
-       <div className="leftSide">
-            <h2>RAYCHELLE'S PORTFOLIO</h2>
-        </div>
-        <div className="rightSide">
-            <ul className='sections'>
-                <li><a href='#About'>About</a></li>
-                <li><a href='#Projects'>Projects</a></li>
-                <li><a href='#Contact' >Contact</a></li>
-            </ul>
-            
-        </div>
-        
-    </div>
-  )
-}
+    <nav className={styles.navbar}>
+      <a href="#hero" className={styles.brand}>
+        R.K.
+      </a>
+      <div className={styles.links}>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Navbar;
